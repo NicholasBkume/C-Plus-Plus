4fc5a54f6526b35792d5e5871976d800ad2f9b49
NicholasBkume <burinicholas@gmail.com>
2024-06-18 01:16:52 -0500

    Update Package.h

--- ConsoleApplication16/Package.h
+++ ConsoleApplication16/Package.h
@@ -4,24 +4,13 @@
 #include <iostream>
 #include <string>
 
-/*
-Your family runs a package delivery services which offer a number of different shipping options, each with specific costs
-associated.Create an inheritance hierarchy to represent various types of packages.Use Package(standard package) as the
-base class of the hierarchy, and then add two derived classes TwoDayPackage and OvernightPackage.In the base class
-(Package), it includes sender and recipient information, package weight in ounces and cost per ounce.Package class has a
-function calcCost() which allows derived classes to override with their own implementations.Derived class TwoDayPackage
-should inherit the functionality from base class, Package, but also includes its own data member that represents a flat fee for
-two day delivery services.OvernightPackage class inherits from Package class and also includes a fee which charges extra fee
-per ounce for overnight delivery services.Both classes will override the calcCost() and print() functions from the base class.
-Your program should contain the following functionality : */
-
-
 using namespace std;
 
 class Package
 {
 
-	//. Create a base class named Package which has the following private data members 
+	//. Create a base class named Package which has the following private data members 
+
 private:
 	double weightInOunce;
 	double costPerOunce;
@@ -38,7 +27,7 @@ public:
 	void setweightInOunce(double W);
 	void setcostPerOunce(double C);
 	void setSenderName(string sname);
-	void setSenderZip(int szip);
+	void setSenderZip(string szip);
 	void setRecipientName(string rname);
 	void setRecipientZip(string rzip);
 
