dd23288e5b942cde41d275fcc1ae2fd50c66e6f8
NicholasBkume <burinicholas@gmail.com>
2024-06-19 14:23:17 -0500

    Update NicholasKumeLottery.cpp

--- C++ side projects/NicholasKumeLottery.cpp
+++ C++ side projects/NicholasKumeLottery.cpp
@@ -1,69 +1,94 @@
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
+
 using namespace std;
-int main(int argc, char *argv[])
+
+const int NUM_DIGITS = 5;
+const int MIN_NUMBER = 0;
+const int MAX_NUMBER = 9;
+
+void generateLotteryNumbers(int lottery[]);
+void getUserNumbers(int user[]);
+void printArray(const int arr[], int size);
+int checkMatches(const int lottery[], const int user[]);
+
+int main()
 {
-       int lottery[5];
-       int user[5];
-       bool match = true;
-       bool notmatch = true;
-       int matchcount = 0;     
-  srand (time(NULL));
-  lottery[0] = rand() % 10,
-  lottery[1] = rand() % 10,
-  lottery[2] = rand() % 10,
-  lottery[3] = rand() % 10,
-  lottery[4] = rand() % 10;
- 
-  for(int i = 0; i < 5; i++)
-  {
-         cout <<"Enter a number in the range between 0 and 9: ";
-cin>>user[i];
-              while (user[i]<0 || user[i]>9)
-              {
-                     cout << "Error!";
-                     cin >> user[i];
-              }
-  }
-cout<<endl<<"Lottery array: ";
-  for(int i = 0; i < 5; i++)
-  {
-         cout<<"|";
-         cout<<lottery[i];
-         cout<<"|";
-  }
-cout<<endl<<endl<<"   User array: ";
-  for(int i = 0; i < 5; i++)
-  {
-         cout<<"|";
-         cout<<user[i];
-         cout<<"|";
-  }
- 
-  for(int i = 0; i < 5; i++)
-  {
-         if(user[i] == lottery[i])
-         {
-               matchcount++;
-               notmatch = false;
-         }
-     
-         if(user[i] != lottery[i])
-         {
-               match = false;
-         }
-  }
-      if(notmatch == true)
-         {
-              cout<<endl<<endl<<"Sorry! No Match."<<endl;
-         }
- 
-         if(match == true)
-         {
-              cout<<endl<<endl<<"Congrats! you are the grand prize winner."<<endl;
-         }
-    if(matchcount != 0)
-       cout<<endl<<endl<<matchcount<<" Matches"<<endl;
-       return 0;
+    int lottery[NUM_DIGITS];
+    int user[NUM_DIGITS];
+    int matchCount = 0;
+
+    srand(time(NULL));
+    generateLotteryNumbers(lottery);
+    getUserNumbers(user);
+
+    cout << "Lottery numbers: ";
+    printArray(lottery, NUM_DIGITS);
+    cout << endl << "Your numbers:    ";
+    printArray(user, NUM_DIGITS);
+
+    matchCount = checkMatches(lottery, user);
+
+    if (matchCount == NUM_DIGITS)
+    {
+        cout << endl << "Congratulations! You are the grand prize winner!" << endl;
+    }
+    else if (matchCount > 0)
+    {
+        cout << endl << matchCount << " match(es)! Well done!" << endl;
+    }
+    else
+    {
+        cout << endl << "Sorry, no matches this time." << endl;
+    }
+
+    return 0;
+}
+
+void generateLotteryNumbers(int lottery[])
+{
+    for (int i = 0; i < NUM_DIGITS; i++)
+    {
+        lottery[i] = rand() % (MAX_NUMBER + 1);  // Generate a random number between 0 and 9
+    }
+}
+
+void getUserNumbers(int user[])
+{
+    for (int i = 0; i < NUM_DIGITS; i++)
+    {
+        cout << "Enter number " << i + 1 << " (between 0 and 9): ";
+        cin >> user[i];
+
+        while (user[i] < MIN_NUMBER || user[i] > MAX_NUMBER)
+        {
+            cout << "Error! Enter a number between 0 and 9: ";
+            cin >> user[i];
+        }
+    }
+}
+
+void printArray(const int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << "| " << arr[i] << " ";
+    }
+    cout << "|";
+}
+
+int checkMatches(const int lottery[], const int user[])
+{
+    int matchCount = 0;
+
+    for (int i = 0; i < NUM_DIGITS; i++)
+    {
+        if (user[i] == lottery[i])
+        {
+            matchCount++;
+        }
+    }
+
+    return matchCount;
 }
